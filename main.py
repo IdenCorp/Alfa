e931615fcba5ba8f93d1956b44660668d72a77d4
d.krylov <d.krylov@systtech.ru>
2022-02-16 18:30:19 +0200

    Первое задание

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-x = 0
+import random
+rand = random.randint(1, 10)
+print("Угадай целое число от 1 до 10")
+
+def user_chose_lit():
+        while True:
+            try:
+
+                user_try = int(input("Твой выбор: "))
+                if 0 < user_try < 10:
+                    return user_try
+                print("от 1 до 10")
+
+            except (TypeError, ValueError):
+                print("Ты ввёл значение, не являющееся целым числом. Сосредоточься")
+
+user = user_chose_lit()
+
+while user != rand:
+    if user > rand:
+        print("Твоё число больше загаданного. Попробуй ещё раз.")
+    elif user < rand:
+        print("Твоё число меньше загаданного. Попробуй ещё раз.")
+    user = user_chose_lit()
+else:
+    print("Ты угадал!")
+print("Загаданное число: ", rand)
